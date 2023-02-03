36e158b72ce69c297e1d6e844185f0f4d8bbab3b
opilane <opilane@HP-CZC2349HTZ>
2023-02-03 11:30:44 +0200

    Add project files.

---
+++ App1/Entry.Page.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entry : ContentPage
+    {
+        Editor ed;
+        public Entry()
+        {
+            ed = new Editor
+            {
+                Placeholder = "Sisesta siia tekst",
+                BackgroundColor = Color.Violet,
+                TextColor = Color.Plum
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { ed },
+                BackgroundColor = Color.Pink
+            };
+            Content = st;
+                
+
+        }
+    }
+}
---
+++ App1/startPage1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class startPage1 : ContentPage
+    {
+        public startPage1()
+        {
+            //InitializeComponent();
+            Button Entry_btn = new Button
+            {
+                Text = "Ava Entry leht",
+                TextColor = Color.Tomato,
+                BackgroundColor = Color.Violet
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { Entry_btn },
+                BackgroundColor = Color.Yellow
+            };
+            Content = st;
+            Entry_btn.Clicked += Entry_btn_Clicked;
+        }
+        private async void Entry_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new startPage1());
+        }
+    }
+        
+        
+
+
+    
+         
+       
+}
